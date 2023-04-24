404b65ccad6257600952dd38c30a4a0281499f6b
veydoge <veydoge@gmail.com>
2023-04-24 12:31:42 +0300

    Добавил комментарии к коду модулей тестирования

--- BottleDmitriev/unit_test/test_F_mail.py
+++ BottleDmitriev/unit_test/test_F_mail.py
@@ -3,9 +3,9 @@ import sys
 sys.path.append("C:/Users/admin/source/repos/BottleDmitriev/BottleDmitriev")
 from myform import *
 
-class Test_F_mail(unittest.TestCase):
+class Test_F_mail(unittest.TestCase): #incorrect mails test module
     def test(self):
-        maillist = ["", "1", "m1@", "@mail", ".com", "hellomymail.com", "testcase()@mail.ru", "mymail @ mail.ru", "mail@mail,ru"]
-        for i in maillist:
+        maillist = ["", "1", "m1@", "@mail", ".com", "hellomymail.com", "testcase()@mail.ru", "mymail @ mail.ru", "mail@mail,ru"] #correct mails list
+        for i in maillist: #test every mail in list
             self.assertFalse(bool(regular_expr(i)))
             
--- BottleDmitriev/unit_test/test_T_mail.py
+++ BottleDmitriev/unit_test/test_T_mail.py
@@ -3,8 +3,8 @@ import sys
 sys.path.append("C:/Users/admin/source/repos/BottleDmitriev/BottleDmitriev")
 from myform import *
 
-class Test_T_mail(unittest.TestCase): 
+class Test_T_mail(unittest.TestCase): #correct mails test module
     def test(self):
-        maillist = ["mymail@mail.ru", "validmail@mail.edu.ru", "valid_mail@mail.check", "123mail@no.hey"]
-        for i in maillist:
+        maillist = ["mymail@mail.ru", "validmail@mail.edu.ru", "valid_mail@mail.check", "123mail@no.hey"] #incorrent mails list
+        for i in maillist: #test every mail in list
             self.assertTrue(bool(regular_expr(i)))
